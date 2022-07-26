9b2922fbb8f00d781891eb0c51553e8343201a07
Your NameKarem <hamze.karim99@gmail.com>
2022-07-26 23:33:36 +0300

    chore: Fetch api

---
+++ src/components/Movies/Movies.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import './Movies.css';
+
+const Movies: React.FC = (data: any) => {
+    const [movies, setMovies] = useState([]);
+    const [searchWord, setSearchWord] = useState("");
+
+    useEffect(() => {
+        getdata();
+    }, [data]);
+
+    const getdata = async () => {
+        await fetch("https://api.tvmaze.com/shows")
+            .then((response) => response.json())
+            .then((res) => setMovies(res))
+            .catch(err=>console.log(err));
+    };
+
+    const Movies: any = movies.filter((obj: any) => {
+        return obj.name.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    return (
+        <div>
+            <div >
+                <h1 className="header"> MOVIES</h1>
+            </div>
+
+            <div >
+                <input
+                    type="text"
+                    placeholder="search"
+                    className="inputbox"
+                    onChange={(e: any) => {
+                        setSearchWord(e.target.value);
+                    }}
+                />
+            </div>
+            <div className="moviesContainer">
+                {Movies.map((movie: any) => (
+                    <div key={movie.id}>
+                        <div className="card">
+                            <div>
+                                <img className="image" src={movie.image.medium} alt="#" />
+                            </div>
+                            <div>
+                                <a href={movie.url} className="titleText" >{movie.name}</a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Movies;
